fa53bfd2393596c3cd1c09ce159f052ded3f68a4
Pit Braunsdorf <pit.braunsdorf@comparex.de>
2017-10-04 10:56:12 +0200

    Database Entity Tags added

---
+++ database-service/Masterarbeit.DatabaseService.Database/Model/Tags.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Masterarbeit.DatabaseService.Database.Model
+{
+    public class Tags
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        public string Tag { get; set; }
+    }
+}
